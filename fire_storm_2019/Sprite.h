6ad039c6a8978f7504d0440d752e2588de7d0c47
Joel Larsson <joellarsson534@gmail.com>
2019-12-12 12:09:46 +0100

    Commented Spritefunctions in the header file

--- fire_storm_2019/Sprite.h
+++ fire_storm_2019/Sprite.h
@@ -2,17 +2,19 @@
 #define SPRITE_H
 #include<SDL.h>
 namespace fs19 {
+	//A general class that all components like player etc will inherit from. Header.
 	class Sprite
 	{
 	public:
 		virtual ~Sprite();
-		virtual void tick() {}
-		virtual void draw() const = 0;
-		virtual void mouseDown(const SDL_Event&) {};
+		virtual void tick() {} //An incremential tick that updates the game with each tick at end of the loop
+		virtual void draw() const = 0; //draws the components
+		virtual void mouseDown(const SDL_Event&) {}; //Whatever happens when you hold the mouse button down.
 		virtual void mouseUp(const SDL_Event&) {};
 		virtual void keyDown(const SDL_Event&) {};
 		virtual void keyUp(const SDL_Event&) {};
-		const SDL_Rect& getRect() const { return rect; }
+		const SDL_Rect& getRect() const { return rect; } // Each Sprite holds a rectangel where its drawn up upon
+														//which coordinates
 	protected:
 		Sprite(int x, int y, int w, int h);
 

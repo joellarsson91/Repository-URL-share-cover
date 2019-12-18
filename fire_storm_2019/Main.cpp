d23e0bd7af80879fddf197ec61cbddf91e914f3b
Leo Blumenberg <leo.kfum@hotmail.com>
2019-12-18 10:09:32 +0100

    Revert "Added background for the new game button"
    This reverts commit 977f3974d02889bbc6ff9618c088af3bfa313c61.

--- fire_storm_2019/Main.cpp
+++ fire_storm_2019/Main.cpp
@@ -2,30 +2,9 @@
 #include <string>
 #include "GameEngine.h"
 #include "Button.h"
-#include "StaticS.h"
-#include "System.h"
-#include <SDL_image.h>
-#include <iostream>
 
 using namespace std;
 using namespace fs19;
-
-class Background : public StaticS {
-
-public:
-	//remove hardcoded size later
-	Background(string s) :StaticS(0,0,800,600) {
-		texture = IMG_LoadTexture(sys.get_ren(), s.c_str());
-	}
-	void draw() const{
-		SDL_RenderCopy(sys.get_ren(), texture, NULL, &getRect());
-
-	}
-private:
-	SDL_Texture* texture;
-
-
-};
 
 	class newGameButton : public Button {
 	public:
@@ -40,9 +19,7 @@ private:
 	int main(int argc, char** argv) {
 		GameEngine ge;
 		newGameButton* ngb = new newGameButton();
-
-		StaticS* bg = new Background("background.png");
-		ge.add(bg);
+		ge.add(ngb);
 		ge.run();
 
 		return 0;

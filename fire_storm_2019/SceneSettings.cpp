089fe4c11d7b5a0fab268b8c5c89c36b911dc944
Noel Hennings <noel.hennings@gmail.com>
2019-12-27 12:53:56 +0100

    Added SceneSettings.cpp and .h

---
+++ fire_storm_2019/SceneSettings.cpp
@@ -0,0 +1,19 @@
+#include "SceneSettings.h"
+#include "Button.h"
+#include <string>
+#include "System.h"
+#include <SDL_image.h>
+#include "Sprite.h"
+#include <SDL.h>
+
+namespace fs19 {
+	//börjat planka från Scene0, kanske är helt fel?
+
+	SceneSettings::~SceneSettings() {
+
+	}
+
+	void SceneSettings::populateSpriteList() {
+
+	}
+}

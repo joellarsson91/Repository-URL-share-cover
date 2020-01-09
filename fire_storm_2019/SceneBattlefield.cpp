851b0e142e16fbf3b60902fa463391da640274a3
Noel Hennings <noel.hennings@gmail.com>
2020-01-09 10:03:44 +0100

    Boulder fixed?

--- fire_storm_2019/SceneBattlefield.cpp
+++ fire_storm_2019/SceneBattlefield.cpp
@@ -14,7 +14,7 @@ namespace fs19 {
 	}
 
 
-	
+
 	class Background : public Sprite {
 	public:
 		//remove hardcoded size later
@@ -32,10 +32,35 @@ namespace fs19 {
 
 	};
 
+	class Boulder : public Sprite {
+	public:
+		static Boulder* getInstance(int x, int y) {
+			return new Boulder(x, y);
+		}
+
+		Boulder(int x, int y) : Sprite(x, y, 40, 40) {
+			texture = IMG_LoadTexture(sys.get_ren(), "pixelBoulder.jpg");
+		}
+
+		void draw() const {
+			SDL_RenderCopy(sys.get_ren(), texture, NULL, &getRect());
+
+		}
+		void tick() {
+			//Kanske måste definieras för att gå snabbare/långsammare
+		};
+
+		~Boulder() {
+		};
+
+	private:
+		SDL_Texture* texture;
+
+	};
 
 	class Player1 : public PlayerSprite {
 	public:
-		Player1() : PlayerSprite(0, 300, 100, 50, "pixelCatapult.png"){}
+		Player1() : PlayerSprite(0, 300, 100, 50, "pixelCatapult.png") {}
 
 
 
@@ -65,7 +90,7 @@ namespace fs19 {
 	class Player2 : public PlayerSprite {
 	public:
 		Player2() : PlayerSprite(700, 300, 100, 50, "pixelCatapult.png") {}
-		
+
 
 
 		void keyDown(const SDL_Event& eve) {
@@ -92,30 +117,7 @@ namespace fs19 {
 
 	};
 
-	class Boulder : public Sprite {
-	public:
-		static Boulder* getInstance(int x, int y) {
-			return new Boulder(x, y);
-		}
 
-		Boulder(int x, int y) : Sprite(x, y, 40, 40) {
-			texture = IMG_LoadTexture(sys.get_ren, "pixelBoulder.jpg");
-		}
-		
-		void draw() const {
-			SDL_RenderCopy(sys.get_ren(), texture, NULL, &getRect());
-
-		}
-		void tick() {
-		//Kanske måste definieras för att gå snabbare/långsammare
-		};
-
-		~Boulder();
-
-	private:
-		SDL_Texture* texture;
-
-	};
 
 
 

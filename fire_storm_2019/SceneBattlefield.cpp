b7e916e2607045f80d41a7cf67c518037f9a1609
Joel Larsson <joellarsson534@gmail.com>
2020-01-10 10:57:27 +0100

    Cooddown on boulder shots made in tick

--- fire_storm_2019/SceneBattlefield.cpp
+++ fire_storm_2019/SceneBattlefield.cpp
@@ -39,7 +39,7 @@ namespace fs19 {
 			return new Boulder(x, y, path);
 		}
 
-		Boulder(int x, int y, std::string path) : Sprite(x, y, 40, 40) {
+		Boulder(int x, int y, std::string path) : Sprite(x, y, 30, 30) {
 			texture = IMG_LoadTexture(sys.get_ren(), "pixelBoulder.jpg");
 		}
 
@@ -50,47 +50,65 @@ namespace fs19 {
 		}
 		//Kanske måste definieras för att gå snabbare/långsammare
 		void tick() {
-			int xtemp = getRect().x;
-			int ytemp = getRect().y;
 
 			//Counter för att ökas för varje gång den studsar.
-
-			if (getRect().x <= 0 || getRect().x >= 800) {
-				ge.remove(this);
-			} else {
 			
-				if (getRect().x < xtemp + 10) {
+				if (getRect().x <= 0 || getRect().x >= 800) {
+					ge.remove(this);
+				}
+				else {
 
-					setPosition(2, -1);
+					if (getRect().x < xtemp + 300) {
+
+						setPosition(5, -3);
+						
+
+					}
+					else if (getRect().x >= xtemp + 300 && getRect().y < ytemp) {
+
+						setPosition(5, 3);
+						
+					}
 
 				}
-				else if (getRect().x >= xtemp + 10 && getRect().y < ytemp) {
-
-					setPosition(2, 1);
-				} 
 				
-			
-			}
 		};
+
+
 
 		~Boulder() {
 		};
 
 	private:
 		SDL_Texture* texture;
-		int counter = 0;
+		int xtemp = getRect().x;
+		int ytemp = getRect().y;
+
 	};
 
 	class Player1 : public PlayerSprite {
 	public:
-		Player1() : PlayerSprite(0, 300, 100, 50, "pixelCatapult.png") {}
+		Player1() : PlayerSprite(0, 300, 50, 25, "pixelCatapult2.png") {}
 
+		void tick() {
+			if (counter>0 && counter<150) {
+				counter++;
+				isReady = false;
+			}
+			else {
+				counter = 0;
+				isReady = true;
+			}
+		}
 
 
 
 		void keyDown(const SDL_Event& eve) {
 
 			const Uint8* currentKeyStates = SDL_GetKeyboardState(NULL);
+
+			
+
 
 			if (currentKeyStates[SDL_SCANCODE_W] && currentKeyStates[SDL_SCANCODE_A]) {
 				setPosition(-3, -3);
@@ -122,21 +140,25 @@ namespace fs19 {
 				setPosition(3, 0);
 
 			}
-			
+
 			if (currentKeyStates[SDL_SCANCODE_F]) {
+				if (isReady) {
 				Sprite* boulder = new Boulder(getRect().x, getRect().y, "pixelBoulder.jpg");
 				ge.add(boulder);
-
+				counter++;
+				
+			}
 			}
 		}
-
-
+	private:
+		bool isReady = true;
+		int counter = 0;
 	};
 
 
 	class Player2 : public PlayerSprite {
 	public:
-		Player2() : PlayerSprite(700, 300, 100, 50, "pixelCatapult.png") {}
+		Player2() : PlayerSprite(700, 300, 50, 25, "pixelCatapult2.png") {}
 
 
 
@@ -193,7 +215,8 @@ namespace fs19 {
 
 		Sprite* background = new Background("battlefieldBackground.png");
 		addSprites(background);
-		addSprites(new Player1());
+		Player1* player1 = new Player1();
+		addSprites(player1);
 		Player2* player2 = new Player2();
 		player2->setIsTurnedLeft(true);
 		addSprites(player2);
